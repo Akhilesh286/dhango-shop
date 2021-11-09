10419ed1d3bf2f48ea0ee3ad3dfb410fe0fd6681
Akhilesh <h3356335@gmail.com>
2021-11-09 22:03:23 +0530

    css added

--- shop/action/home.py
+++ shop/action/home.py
@@ -2,15 +2,19 @@ def home (request,products) :
   text1 = request.GET
   name = text1.get ("name")
   print (products)
-  c = text1.get ('c')
-  h = 'bbb'
-  if c=='c':
-    h = "aaaaa"
+  colour = text1.get ("colour")
+  print (colour)
+  button = text1.get ('button')
+  buttona= 'button'
+  if button=='button':
+    buttona = "button action"
   hello = 'hello'
   if name == "a":
     hello= ' hai'
   hanuman = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4pd-YZx4-hboAHx8a8vKSFJfiIHvyxp2njA&usqp=CAU'
-  data = {'products':products,'text':name,'hello':hello , 'image': hanuman,'h':h }
+  data = {'products':products,'text':name,'hello':hello , 'image': hanuman,'button':buttona ,
+    'colour' : colour
+  }
     
   return data
     

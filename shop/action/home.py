68817c77a7c5377216c9bfc9517213d1e8d82837
Akhilesh <h3356335@gmail.com>
2021-11-10 13:07:16 +0530

    color added

--- shop/action/home.py
+++ shop/action/home.py
@@ -1,20 +1,22 @@
 def home (request,products) :
-  text1 = request.GET
-  name = text1.get ("name")
+  req = request.GET
   print (products)
-  colour = text1.get ("colour")
-  print (colour)
-  button = text1.get ('button')
-  buttona= 'button'
-  if button=='button':
-    buttona = "button action"
-  hello = 'hello'
-  if name == "a":
-    hello= ' hai'
-  hanuman = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4pd-YZx4-hboAHx8a8vKSFJfiIHvyxp2njA&usqp=CAU'
-  data = {'products':products,'text':name,'hello':hello , 'image': hanuman,'button':buttona ,
-    'colour' : colour
-  }
+  #-----------colour--------------#
+ 
+  c = 'light'
+  # data in variable
+  dark = req.get ('dark')
+  colour = "#1e2526"
+  if dark == 'dark' :
+    colour="rgb(24,27,28)"
+  elif dark == 'light':
+    colour ="rgb(235,240,241)"
+    c= 'dark'
+  #-----------end colour--------------
+  data = {
+   'colour' :colour,'c':c,
+    }
+  
     
   return data
     
